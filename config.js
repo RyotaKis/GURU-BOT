f7293eda93d8f760d55c799b9da9c67751a01eb8
RyotaKis <159064442+RyotaKis@users.noreply.github.com>
2024-02-11 14:39:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'Shadow_06'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'Ch_Ph_06' 
+global.author = '@Charbel_Phanel_06_🖖🏾' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
